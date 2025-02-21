a1a54f0490067e3a84fd113b087a0b43b6846904
Timekilla-tech <ffjzff@gmail.com>
2025-02-21 10:20:11 +0800

    add shapes

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,12 +1,14 @@
 package edu.cmu.cs.cs214.lab02;
 
+import edu.cmu.cs.cs214.lab02.shapes.Circle;
 import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
 
 public class Main {
     public static void main(String[] args) {
         Rectangle rectangle = new Rectangle(2, 3);
+        Circle circle = new Circle(2);
+        Renderer renderer = new Renderer(circle);
 
-        Renderer renderer = new Renderer(rectangle);
 
         renderer.draw();
     }
--- java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Circle.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Circle.java
@@ -1,13 +1,14 @@
 package edu.cmu.cs.cs214.lab02.shapes;
 
 public class Circle implements Shape {
-    public double radius;
+    private double radius;
 
     public Circle(double radius) {
         this.radius = radius;
     }
-
+    @Override
     public double getArea() {
         return Math.PI * radius * radius;
     }
+    public double getRadius() {return radius;}
 }
